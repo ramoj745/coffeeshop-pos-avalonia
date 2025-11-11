804a54e411a795c1aa0f8b1195ec87d92c676f3a
ramoj745 <ramoj745@gmail.com>
2025-11-11 18:22:54 +0800

    Handle nullable

--- Models/Beverage.cs
+++ Models/Beverage.cs
@@ -3,10 +3,10 @@ namespace CoffeeShopPOS.Models
     // Base class for all beverage types (BlendedCoffee, IcedCoffee, HotCoffee)
     public abstract class Beverage
     {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public decimal BasePrice { get; set; }
-        public string Category { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public decimal BasePrice { get; set; } 
+        public string Category { get; set; } = string.Empty;
         
         // each Beverage type will have different base prices
         public abstract decimal CalculatePrice(string size);
